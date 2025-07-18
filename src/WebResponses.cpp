4cc423bc31a9af2ec4842071e53ca593f958d8d5
Jose M. Piñeiro <diosmedepaciencia@gmail.com>
2025-07-18 03:47:11 +0200

    Refactor AsyncFileResponse: remove String _path
    This refactor eliminates the private String _path member from the AsyncFileResponse class, replacing its functionality with a fixed-size char gzPath[MAX_PATH] buffer insife of function.
    
    The use of Arduino's dynamic String class has known drawbacks in memory-constrained or real-time systems, particularly due to heap fragmentation and unpredictable allocation behavior. By switching to a statically allocated C-style buffer, the implementation now benefits from improved memory determinism, reduced fragmentation risk, and overall lower memory overhead.
    In addition to reducing memory fragmentation and improving stability in embedded environments, this change also yields better execution speed.
    This change maintains behavioral equivalence.

--- src/WebResponses.cpp
+++ src/WebResponses.cpp
@@ -701,15 +701,21 @@ void AsyncFileResponse::_setContentTypeFromPath(const String &path) {
  */
 AsyncFileResponse::AsyncFileResponse(FS &fs, const String &path, const char *contentType, bool download, AwsTemplateProcessor callback)
   : AsyncAbstractResponse(callback) {
+  constexpr size_t MAX_PATH = 128;
+  const size_t GZ_SUFFIX_LEN = sizeof(T__gz);
+
   // Try to open the uncompressed version first
   _content = fs.open(path, fs::FileOpenMode::read);
   if (_content.available()) {
-    _path = path;
     _contentLength = _content.size();
   } else {
     // Try to open the compressed version (.gz)
-    _path = path + asyncsrv::T__gz;
-    _content = fs.open(_path, fs::FileOpenMode::read);
+    size_t pathLen = path.length();
+    char gzPath[MAX_PATH]; // Safe buffer size for ESP32 paths
+    memcpy(gzPath, path.c_str(), pathLen);
+    memcpy(gzPath + pathLen, asyncsrv::T__gz, GZ_SUFFIX_LEN);
+
+    _content = fs.open(gzPath, fs::FileOpenMode::read);
     _contentLength = _content.size();
 
     if (_content.seek(_contentLength - 8)) {
@@ -758,8 +764,7 @@ AsyncFileResponse::AsyncFileResponse(FS &fs, const String &path, const char *con
 AsyncFileResponse::AsyncFileResponse(File content, const String &path, const char *contentType, bool download, AwsTemplateProcessor callback)
   : AsyncAbstractResponse(callback) {
   _code = 200;
-  _path = path;
-
+  
   if (!download && String(content.name()).endsWith(T__gz) && !path.endsWith(T__gz)) {
     addHeader(T_Content_Encoding, T_gzip, false);
     _callback = nullptr;  // Unable to process gzipped templates
