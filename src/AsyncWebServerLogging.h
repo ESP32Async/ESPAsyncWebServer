f13f005be992e93b707a998ba6440b126b346933
Mitch Bradley <wmb@firmworks.com>
2026-04-06 12:32:32 -1000

    HOST versions of logging.

--- src/AsyncWebServerLogging.h
+++ src/AsyncWebServerLogging.h
@@ -32,11 +32,61 @@
 #define async_ws_log_v(format, ...) log_v(format, ##__VA_ARGS__)
 
 #elif defined(HOST)
+// Arduino-Emulator has Serial, but it does not have Serial.printf, which
+// is not a member of the Arduino Core API Print class.
+
+// define log levels
+#define ASYNC_WS_LOG_NONE    0 /*!< No log output */
+#define ASYNC_WS_LOG_ERROR   1 /*!< Critical errors, software module can not recover on its own */
+#define ASYNC_WS_LOG_WARN    2 /*!< Error conditions from which recovery measures have been taken */
+#define ASYNC_WS_LOG_INFO    3 /*!< Information messages which describe normal flow of events */
+#define ASYNC_WS_LOG_DEBUG   4 /*!< Extra information which is not necessary for normal use (values, pointers, sizes, etc). */
+#define ASYNC_WS_LOG_VERBOSE 5 /*!< Verbose information for debugging purposes */
+#define ASYNC_WS_LOG_MAX     6 /*!< Number of levels supported */
+// set default log level
+#ifndef ASYNCWEBSERVER_LOG_LEVEL
+#define ASYNCWEBSERVER_LOG_LEVEL ASYNC_WS_LOG_INFO
+#endif
+// error
+#if ASYNCWEBSERVER_LOG_LEVEL >= ASYNC_WS_LOG_ERROR
+#define async_ws_log_e(format, ...)                                               \
+  ::printf("E async_ws %s() %d: " format, __FUNCTION__, __LINE__, ##__VA_ARGS__); \
+  ::putchar('\n');
+#else
 #define async_ws_log_e(format, ...)
+#endif
+// warn
+#if ASYNCWEBSERVER_LOG_LEVEL >= ASYNC_WS_LOG_WARN
+#define async_ws_log_w(format, ...)                                               \
+  ::printf("W async_ws %s() %d: " format, __FUNCTION__, __LINE__, ##__VA_ARGS__); \
+  ::putchar('\n');
+#else
 #define async_ws_log_w(format, ...)
+#endif
+// info
+#if ASYNCWEBSERVER_LOG_LEVEL >= ASYNC_WS_LOG_INFO
+#define async_ws_log_i(format, ...)                                               \
+  ::printf("I async_ws %s() %d: " format, __FUNCTION__, __LINE__, ##__VA_ARGS__); \
+  ::putchar('\n');
+#else
 #define async_ws_log_i(format, ...)
+#endif
+// debug
+#if ASYNCWEBSERVER_LOG_LEVEL >= ASYNC_WS_LOG_DEBUG
+#define async_ws_log_d(format, ...)                                               \
+  ::printf("D async_ws %s() %d: " format, __FUNCTION__, __LINE__, ##__VA_ARGS__); \
+  ::putchar('\n');
+#else
 #define async_ws_log_d(format, ...)
+#endif
+// verbose
+#if ASYNCWEBSERVER_LOG_LEVEL >= ASYNC_WS_LOG_VERBOSE
+#define async_ws_log_v(format, ...)                                               \
+  ::printf("V async_ws %s() %d: " format, __FUNCTION__, __LINE__, ##__VA_ARGS__); \
+  ::putchar('\n');
+#else
 #define async_ws_log_v(format, ...)
+#endif
 
 /**
  * Raspberry Pi Pico specific configurations
@@ -150,6 +200,7 @@
  * Arduino specific configurations
  */
 #elif defined(ARDUINO)
+
 #if defined(USE_ESP_IDF_LOG)
 #include <esp_log.h>
 #define async_ws_log_e(format, ...) ESP_LOGE("async_ws", "%s() %d: " format, __FUNCTION__, __LINE__, ##__VA_ARGS__)
