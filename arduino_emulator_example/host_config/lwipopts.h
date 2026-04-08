6580dd163929ae40e5923ee717f76b5c70ce2e33
Mathieu Carbou <mathieu.carbou@gmail.com>
2026-04-08 16:23:35 +0200

    Try fix CI on linux (works on mac)

--- arduino_emulator_example/host_config/lwipopts.h
+++ arduino_emulator_example/host_config/lwipopts.h
@@ -5,3 +5,7 @@
 #define LWIP_NETCONN 0
 #define LWIP_TCP     1
 #define LWIP_IPV6    1
+
+#if defined(__linux__) && !defined(LWIP_DONT_PROVIDE_BYTEORDER_FUNCTIONS)
+#define LWIP_DONT_PROVIDE_BYTEORDER_FUNCTIONS 1
+#endif
